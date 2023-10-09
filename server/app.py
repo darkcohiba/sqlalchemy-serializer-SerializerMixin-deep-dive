364fb02f7c548c25d1ced56e42a8c536c863cd28
Sam Waters <samuel.waters@gmail.com>
2023-10-08 20:21:10 -0600

    feat: first route

--- server/app.py
+++ server/app.py
@@ -3,19 +3,33 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import make_response, jsonify, request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Projects, Tasks, Engineers
 
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Phase 4 Project Server</h1>'
+
+@app.route("/projects")
+def projectsAll():
+    if request.method == "GET":
+        projs = [p.to_dict() for p in Projects.query.all()]
+        return make_response(projs, 200)
+        # all_planet = Planet.query.all()
+        # all_planet_dict = []
+        # for planet in all_planet:
+        #     planet.serialize_rules = ('-missions',)
+        #     all_planet_dict.append(planet.to_dict())
+        # return make_response(all_planet_dict,200)
+
+
 
 
 if __name__ == '__main__':
